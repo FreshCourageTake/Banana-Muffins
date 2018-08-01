e54870dec0f11a3e6f3c212411c353c990a7a1aa
unknown <andrewsgarver@gmail.com>
2018-07-31 18:18:12 -0600

    fully responsive front page

--- site/assets/cache/FileCompiler/site/templates/_head.php
+++ site/assets/cache/FileCompiler/site/templates/_head.php
@@ -6,6 +6,7 @@
 	<title><?php echo $page->title; ?></title>
 	<meta name="description" content="<?php echo $pages->get("/")->summary; ?>" />
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=Cinzel|Raleway" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="<?php echo $config->urls->templates?>styles/main.css" />
 </head>
 <body>
